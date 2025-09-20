031894d61901535b06ec4f00c799adad81964e2a
YagoGefaellOficial <yaguetegpl@gmail.com>
2025-09-20 13:55:27 +0200

    Set up project structure

---
+++ src/main/java/io/github/yagogefaell/asaltamontes/AsaltamontesApplication.java
@@ -0,0 +1,13 @@
+package io.github.yagogefaell.asaltamontes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsaltamontesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsaltamontesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/yagogefaell/asaltamontes/AsaltamontesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yagogefaell.asaltamontes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsaltamontesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
